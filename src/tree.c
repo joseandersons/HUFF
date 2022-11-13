fda48532fefa2564405eb16e7c89b0e7fb0f6ef0
José Anderson <joseandersonsilv4@gmail.com>
2022-11-12 21:36:43 -0300

    att2

--- src/tree.c
+++ src/tree.c
@@ -50,7 +50,7 @@ TREE *mount_tree(struct lists_s *list){
 
         enfilerar(list, new_tree);
 
-        if(list->size)break;
+        if(list->size == 1)break;
     }
     TREE *tree_huff = top(list);
     return tree_huff;
